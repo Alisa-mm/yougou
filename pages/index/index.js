fdc3c3ec5e626329d7c01f443907b1edb6f4b2ba
lalala <iuyangdaxia@163.com>
2020-02-28 20:22:53 +0800

    导航菜单渲染完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,36 @@ Page({
  data:{
   //  轮播图数据
   banners:[],
+  //菜单数据
+  menu:[]
  },
  onLoad(){
   //  请求轮播图接口
   request({
     url:'/home/swiperdata'
-  }).then(res=>{
+    }).then(res=>{
     // console.log(res)
     const {message}=res.data;
     // 赋值给banners
     this.setData({
       banners:message
+    })
+  }),
+  //请求分类菜单
+  request({
+    url:'/home/catitems'
+  }).then(res=>{
+    // console.log(res)
+    const{message}=res.data;
+    // 循环添加跳转链接
+    const newData = message.map((v,i)=>{
+      if(i==0){
+        v.url = "/pages/category/index"
+      }
+      return v;
+    })
+    this.setData({
+      menu: newData
     })
   })
  }
