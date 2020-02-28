1ade57c6cb46050fb4bb2da43716d5f1c03ba6d1
lalala <iuyangdaxia@163.com>
2020-02-28 19:40:40 +0800

    tabBar配置和首页轮播图完成

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-//app.js
+//引入封装好的request
+import request from "./utils/request.js";
 App({
   onLaunch: function () {
-   
+   //基准路径
+    request.defaults.baseURL ="https://api-hmugo-web.itheima.net/api/public/v1"
+
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,21 @@
+// 引入request
+import request from "../../utils/request.js"
 Page({
- 
+ data:{
+  //  轮播图数据
+  banners:[],
+ },
+ onLoad(){
+  //  请求轮播图接口
+  request({
+    url:'/home/swiperdata'
+  }).then(res=>{
+    // console.log(res)
+    const {message}=res.data;
+    // 赋值给banners
+    this.setData({
+      banners:message
+    })
+  })
+ }
 })
