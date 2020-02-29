8420ba26634829527eb9350687aeb06fae8c3f81
lalala <iuyangdaxia@163.com>
2020-03-01 00:30:35 +0800

    自定义搜索组件以及分类页完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,39 @@
-// pages/category/index.js
+import request from "../../utils/request.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 记录当前点击了哪个菜单项
+    current:0,
+    //数据列表
+    list:[]
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+     * 生命周期函数--监听页面加载
+     */
+  onLoad(){
+    //发送请求
+    request({
+      url: '/categories'
+    }).then(res=>{
+      console.log(res)
+      const {message}=res.data;
+      this.setData({
+        list:message
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击切换左侧菜单栏时触发
+  handeleClick(e){
+    const { index } = e.currentTarget.dataset;
+    //保存当前索引
+    this.setData({
+      current:index
+    })
   }
 })
