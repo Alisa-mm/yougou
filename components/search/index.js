993cde40e0fff1af9113b3ebfcdca704b94464ec
lalala <iuyangdaxia@163.com>
2020-03-03 20:19:15 +0800

    添加搜索列表页搜索框样式

--- components/search/index.js
+++ components/search/index.js
@@ -8,9 +8,15 @@ Component({
     keyword:{
       type:String,
       value:"搜索"
+    },
+    align:{
+      type:String,
+      value:"center"
     }
   },
-
+  // 设置外部拓展样式
+  // background样式（传给搜索列表的）align输入框的文字居中还是左对齐 还是右对齐
+  externalClasses: ['background', "align"],
   /**
    * 组件的初始数据
    */
